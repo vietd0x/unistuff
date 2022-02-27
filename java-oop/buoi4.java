7244346cae7237848403d96a30d16c554a7db123
VietDo <76168059+vietd0x@users.noreply.github.com>
2022-02-27 10:22:37 +0700

    Delete buoi4.java

--- java-oop/buoi4.java
+++
@@ -1,9 +0,0 @@
-
-public class buoi4 {
-	public static void main(String[] args) {
-		Fraction a = new Fraction(2, 4);
-		Fraction b = new Fraction(3, 8);
-		a.normalize();
-		System.out.println(a.add(b).fracStr);
-	}
-}
